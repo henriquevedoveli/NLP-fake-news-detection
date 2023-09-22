0bedda0f19c0f94009769cda2258c139c3187fcd
Henrique Vedoveli <61060105+henriquevedoveli@users.noreply.github.com>
2023-09-22 08:31:47 -0300

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -3,17 +3,5 @@ import pickle
 def predict(data):
     filename = 'logistic_regression.sav'
 
-    print(data)
-
     loaded_model = pickle.load(open(filename, 'rb'))
     return loaded_model.predict([data])
-
-filename = 'logistic_regression.sav'
-
-loaded_model = pickle.load(open(filename, 'rb'))
-
-text = 'some text to predict'
-
-res = loaded_model.predict([text])
-
-print(res)
